662e4a9bea1b8965fa0ea8bc8dc7023828f5f4ab
phoenixfuryy <deepthirishitha.pamisetty@gmail.com>
2026-02-11 20:20:42 -0800

    Added LC1480

---
+++ LC1480-RunningSumOf1DArray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i-1]
+        print(nums)
+
+        """for i, el in enumerate(nums):
+            if i>=1:
+                nums[i] = nums[i]+nums[i-1]
+            print(nums)"""
+    
+""" 
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+   
+"""
